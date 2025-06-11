ac88b735912acc6b8c460fa3b03423229bd91c1e
Everson Sousa <eversonsousa@gmail.com>
2025-06-11 19:08:23 -0300

    inserindo logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
       <Container>
         <Heading>Menu</Heading>
