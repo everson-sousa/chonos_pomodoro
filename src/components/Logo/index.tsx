0c44c057c572327b38bd90df8063e42758751631
Everson Sousa <eversonsousa@gmail.com>
2025-06-12 10:26:47 -0300

    logo finalizado

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -5,7 +5,7 @@ export function Logo() {
   return (
     <div className={styles.logo}>
       <a className={styles.logoLink} href='#'>
-        <TimerIcon size={70} />
+        <TimerIcon />
         <span>Chronos</span>
       </a>
     </div>
