9c2f98f79b55a95e5e04a27992688754b51c7559
Everson Sousa <eversonsousa@gmail.com>
2025-06-18 10:08:09 -0300

    finalizando aula 27

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './styles/theme.css';
 import './styles/global.css';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -23,8 +24,7 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <label htmlFor='input'>task</label>
-            <input id='input' type='text' />
+            <DefaultInput labelText='task' id='meuInput' type='text' />
           </div>
 
           <div className='formRow'>
