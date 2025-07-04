e1d0b9d0299cfc2472287bdd9b32737562ce79bc
Everson Sousa <eversonsousa@gmail.com>
2025-07-04 12:06:53 -0300

    useEffetc temas

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 export function Menu() {
@@ -9,8 +9,32 @@ export function Menu() {
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
-    console.log('Clicado', Date.now());
+    event.preventDefault(); // não siga o link
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
   }
+  //useEffect(() => {});
+
+  // useEffect(() => {
+  //   console.log('useEffect sem dependencias',Date.now());
+  // });
+
+  //    useEffect(() => {
+  //   console.log('', Date.now());
+  // }, []);
+
+  useEffect(() => {
+    console.log('Theme mudou', theme, Date.now());
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log('Este componente será atualizado!');
+    };
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
       <h1>{theme}</h1>
