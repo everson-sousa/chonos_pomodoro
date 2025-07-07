126e487472ee6ce970e2648c240a94ac92ad7bac
Everson Sousa <eversonsousa@gmail.com>
2025-07-07 17:27:29 -0300

    configurando tema light e dark

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,43 +1,45 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
 import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
 
   function handleThemeChange(
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
   ) {
-    event.preventDefault(); // não siga o link
+    event.preventDefault();
 
     setTheme(prevTheme => {
       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
       return nextTheme;
     });
   }
-  //useEffect(() => {});
-
-  // useEffect(() => {
-  //   console.log('useEffect sem dependencias',Date.now());
-  // });
-
-  //    useEffect(() => {
-  //   console.log('', Date.now());
-  // }, []);
 
   useEffect(() => {
-    console.log('Theme mudou', theme, Date.now());
     document.documentElement.setAttribute('data-theme', theme);
-
-    return () => {
-      console.log('Este componente será atualizado!');
-    };
+    localStorage.setItem('theme', theme);
   }, [theme]);
 
   return (
     <nav className={styles.menu}>
-      <h1>{theme}</h1>
       <a
         className={styles.menuLink}
         href='#'
@@ -72,7 +74,7 @@ export function Menu() {
         title='Alterar Tema'
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
