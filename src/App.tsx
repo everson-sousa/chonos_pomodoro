1444def1232c8e0d880060ac950c8aeed768b2c8
Everson Sousa <eversonsousa@gmail.com>
2025-07-10 11:17:27 -0300

    incrmentando pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,7 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
 import './styles/theme.css';
 import './styles/global.css';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { Footer } from './components/Footer';
-import { PlayCircleIcon } from 'lucide-react';
+import { Home } from './pages/Home';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='task'
-              id='meuInput'
-              type='text'
-              placeholder='Digite Algo'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <button>Enviar</button>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-          </div>
-        </form>
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,28 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+
+export function MainForm() {
+  return (
+    <form className='form' action=''>
+      <div className='formRow'>
+        <DefaultInput
+          labelText='task'
+          id='meuInput'
+          type='text'
+          placeholder='Digite Algo'
+        />
+      </div>
+      <div class='formRow'>
+        <p>Lorem ipsum...</p>
+      </div>
+      <div className='formRow'>
+        <Cycles />
+      </div>
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
