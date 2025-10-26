e9ce2e5ece65d663541e7d242b7762c872f0fa45
Everson Sousa <eversonsousa@gmail.com>
2025-10-25 21:02:52 -0300

    aula50

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,24 +2,10 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import { useTaskContext } from '../../contexts/TaskContext';
 
 export function MainForm() {
-  const { setState } = useTaskContext();
-
-  function handleClick() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        formattedSecondsRemaining: '22:00',
-      };
-    });
-  }
   return (
     <form className='form' action=''>
-      <button onClick={handleClick} type='button'>
-        Clique
-      </button>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
