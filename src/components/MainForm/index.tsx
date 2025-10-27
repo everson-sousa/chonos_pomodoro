52ef8dc32b2e2228cabd280ac83740a1856e8849
Everson Sousa <eversonsousa@gmail.com>
2025-10-26 21:54:13 -0300

    aula 52

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,8 +2,10 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useState } from 'react';
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState('');
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     console.log('Okay');
@@ -16,6 +18,8 @@ export function MainForm() {
           id='meuInput'
           type='text'
           placeholder='Digite Algo'
+          value={taskName}
+          onChange={e => setTaskName(e.target.value)}
         />
       </div>
       <div className='formRow'>
