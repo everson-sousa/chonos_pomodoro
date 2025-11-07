54f22bb96b1567aa341c068173d98be39dcee4e9
Everson Sousa <eversonsousa@gmail.com>
2025-11-07 19:05:17 -0300

    aula 57 duration

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -31,7 +31,7 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
+      duration: state.config[nextCycleType],
       type: nextCycleType,
     };
 
