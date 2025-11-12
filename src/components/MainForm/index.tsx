d65c20b7e17d3c56533e66d1d124ab1d975a02f5
Everson Sousa <eversonsousa@gmail.com>
2025-11-12 08:52:45 -0300

    interrupt task e bug

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -49,6 +49,16 @@ export function MainForm() {
       };
     });
   }
+  function handleInterruptTask() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: '00:00',
+      };
+    });
+  }
 
   return (
     <form onSubmit={handleCreateNewTask} className='form' action=''>
@@ -72,20 +82,25 @@ export function MainForm() {
         </div>
       )}
       <div className='formRow'>
-        {!state.activeTask ? (
+        {!state.activeTask && (
           <DefaultButton
             aria-label='Iniciar nova tarefa'
             title='Iniciar nova tarefa'
             type='submit'
             icon={<PlayCircleIcon />}
+            key='Este é o botão de submit'
           />
-        ) : (
+        )}
+
+        {!!state.activeTask && (
           <DefaultButton
             aria-label='Interromper tarefa atual'
             title='Interromper tarefa atual'
             type='button'
             color='red'
             icon={<StopCircleIcon />}
+            onClick={handleInterruptTask}
+            key='Não enviar formulário'
           />
         )}
       </div>
